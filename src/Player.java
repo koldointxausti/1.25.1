5e6cc8c16725c190c7b80ab9e5be74580407b776
koldointxaustiusername <ik013043z1@H013043.NET>
2018-11-09 10:34:24 +0100

    3

---
+++ src/Player.java
@@ -0,0 +1,27 @@
+public class Player {
+	
+	private String name;
+	private int age;
+	private String position;
+	
+	public String getName() {
+		return this.name;
+	}
+	public void setName(String name) {
+		this.name= name;
+	}
+	
+	public int getAge() {
+		return this.age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public String getPosition() {
+		return this.position;
+	}
+	public void setPosition(String position) {
+		this.position=position;
+	}
+}
--- src/Team.java
+++ src/Team.java
@@ -1,9 +1,8 @@
 public class Team {
 	
 	private String nationality;
-	private String name;
-	private int age;
-	private String position;
+	private String stadium;
+	private int capacity;
 	
 	public String getNationality() {
 		return this.nationality;
@@ -12,24 +11,17 @@ public class Team {
 		this.nationality = nationality;
 	}
 	
-	public String getName() {
-		return this.name;
+	public String getStadium() {
+		return this.stadium;
 	}
-	public void setName(String name) {
-		this.name= name;
-	}
-	
-	public int getAge() {
-		return this.age;
-	}
-	public void setAge(int age) {
-		this.age = age;
+	public void setStadium(String stadium) {
+		this.stadium = stadium;
 	}
 	
-	public String getPosition() {
-		return this.position;
+	public int getCapacity() {
+		return this.capacity;
 	}
-	public void setPosition(String position) {
-		this.position=position;
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
 	}
 }
