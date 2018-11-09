4c78253abf44ed1c38544b5984655ff11845b2d2
koldointxaustiusername <ik013043z1@H013043.NET>
2018-11-09 10:12:53 +0100

    1

---
+++ src/FootballMatch.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class FootballMatch {
+
+	private String localTeam;
+	private String visitorTeam;
+	private int goalsLocal;
+	private int goalsVisitor;
+
+	public String getLocalTeam() {
+		return this.localTeam;
+    }
+
+	public void setLocalTeam(String LocalTeam) {
+		this.localTeam = LocalTeam;
+	}
+
+	public String getVisitorTeam() {
+		return this.visitorTeam;
+	}
+
+	public void setVisitorTeam(String visitorTeam) {
+		this.visitorTeam = visitorTeam;
+	}
+
+	public int getGoalsLocal() {
+		return this.goalsLocal;
+	}
+
+	public void setGoalsLocal(int goalsLocal) {
+		this.goalsLocal = goalsLocal;
+	}
+
+	public int getGoalsVisitor() {
+		return this.goalsVisitor;
+	}
+
+	public void setGoalsVisitor(int goalsVisitor) {
+		this.goalsVisitor=goalsVisitor;
+	}
+}
